123583986ef71c26a6a177066a26680d2cf73f2b
crestfalln <guptahman01@gmail.com>
2020-12-31 20:09:58 +0530

    Question 43 update

--- 43/main.cpp
+++ 43/main.cpp
@@ -1,30 +1,78 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
-bool lookup[10] = {0};
-int input = 3;
-void premutations(int stage = 0, long num = 0)
+
+bool number[10] = {0};
+int prime[] = {2, 3, 5, 7, 11, 13, 17};
+int count = 0;
+long result = 0;
+
+void check(vector<int> perm)
 {
-    if (stage == 8)
+    bool chec = false;
+    for (int i = 0; i < sizeof(prime) / sizeof(int); i++)
     {
-        cout << num << endl;
-        return;
-    }
-    for (int i = 1; i <= 9; i++)
-    {
-        if (!lookup[i])
+        for (int i = 0; i <= 6; i++)
         {
-            num = num * 10 + i;
-            lookup[i] = true;
-            premutations(stage + 1, num);
-        }
-        else{
-        lookup[i] = false;
-        num = num/10; 
-        return;
+            if ((perm[i + 1] * 100 + perm[i + 2] * 10 + perm[i + 3]) % prime[i] == 0)
+            {
+                chec = true;
+            }
+            else
+            {
+                chec = false;
+                break;
+            }
         }
     }
+    if (chec)
+    {
+        long tmp = 0;
+        for (int i = 0; i < perm.size(); i++)
+        {
+            tmp = tmp * 10 + perm[i];
+        }
+        result += tmp;
+    }
+}
+void permu(int stage = 0, vector<int> perm = {})
+{
+    bool tmp = 0;
+    if (stage == 10)
+    {
+        check(perm);
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < sizeof(number) / sizeof(bool); i++)
+        {
+            if (stage == 0 & i == 0)
+            {
+                continue;
+            }
+            if (!number[i])
+            {
+                if (tmp)
+                {
+                    number[*(perm.end() - 1)] = false;
+                    perm.pop_back();
+                    tmp = 0;
+                }
+                number[i] = true;
+                perm.push_back(i);
+                tmp = 1;
+                permu(stage + 1, perm);
+            }
+        }
+    }
+    number[*(perm.end() - 1)] = false;
+    perm.pop_back();
+    return;
 }
 int main()
 {
-    premutations();
+    permu();
+    cout << result;
 }
